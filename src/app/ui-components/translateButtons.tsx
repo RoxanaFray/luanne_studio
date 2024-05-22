2b21a6832ada3a91b9f203ba5dc4a125bf1f2c82
Tanya <roxxxanafray@gmail.com>
2024-05-22 15:35:25 +0300

    added transition to the arrow

--- src/app/ui-components/translateButtons.tsx
+++ src/app/ui-components/translateButtons.tsx
@@ -3,9 +3,8 @@ import i18n from "../i18n";
 import useLocalStorage from "../hooks/useLocalStorage";
 import { useTranslation } from "next-i18next";
 import "../globals.css";
-import { Button, ButtonGroup, Image } from "@nextui-org/react";
-import { useEffect, useState } from "react";
-import Arrow from "../../utils/arrow.svg";
+import { Button } from "@nextui-org/react";
+import { useState } from "react";
 
 export default function TranslateButtons() {
   const { t }: any = useTranslation();
@@ -21,16 +20,11 @@ export default function TranslateButtons() {
     openSelect(false);
   };
 
-  const moveSelect = () => {
-    const element = document.getElementById("arrow1");
-    element?.classList.add("rotated");
-  };
-
   return (
     <div className="hidden lg:flex flex-col justify-end fixed gap-1 top-8 right-8">
       <Button
         isIconOnly
-        onClick={moveSelect}
+        onClick={() => openSelect(!isSelectOpened)}
         variant="light"
         data-hover="hovered"
         disableRipple={true}
@@ -39,45 +33,35 @@ export default function TranslateButtons() {
         <span className="font-MPlusMedium text-xs text-default-500 text-black/80 pr-2">
           {t(language)}
         </span>
-        {/* {isSelectOpened ? ( */}
         <span
           id="arrow"
-          className="arrow-s font-MPlusMedium select-arrow-down text-xs text-default-500 text-black/80 rotate-90"
+          className={
+            (isSelectOpened ? "rotated-up" : "rotated-down") +
+            " font-MPlusMedium select-arrow-down text-xs text-default-500 text-black/80"
+          }
         >
           ❮
         </span>
-        {/* ) : (
-          <span
-            id="arrow1"
-            className="arrow-s font-MPlusMedium select-arrow-up text-xs text-default-500 text-black/80 rotate-90"
-          >
-            ❯ */}
-        {/* </span> */}
-        {/* )} */}
       </Button>
-      {lans.map((item, index) =>
-        item == language ? (
-          <div key="index" />
-        ) : (
-          <Button
-            key={index}
-            className={
-              isSelectOpened
-                ? "block border-1 border-black/40 rounded py-1 px-2 bg-background-grey"
-                : "hidden"
-            }
-            isIconOnly
-            variant="solid"
-            data-hover="hovered"
-            disableRipple={true}
-            onClick={() => changeLanguage(item)}
-          >
-            <span className="font-MPlusMedium text-xs text-default-500 text-black/80">
-              {t(item)}
-            </span>
-          </Button>
-        ),
-      )}
+      {lans.map((item, index) => (
+        <Button
+          key={index}
+          className={
+            isSelectOpened && item != language
+              ? "block border-1 border-black/40 rounded py-1 px-2 bg-background-grey "
+              : "hidden"
+          }
+          isIconOnly
+          variant="solid"
+          data-hover="hovered"
+          disableRipple={true}
+          onClick={() => changeLanguage(item)}
+        >
+          <span className="font-MPlusMedium text-xs text-default-500 text-black/80">
+            {t(item)}
+          </span>
+        </Button>
+      ))}
     </div>
   );
 }
