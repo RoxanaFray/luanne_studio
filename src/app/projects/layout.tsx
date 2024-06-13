a6e6338e2bad7fd600877d2e4967f0f9f749e6dd
Tanya <roxxxanafray@gmail.com>
2024-06-13 15:16:01 +0300

    added scrollshadow for right side of project page and removed common scroll

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { ScrollShadow } from "@nextui-org/react";
 import { Inter } from "next/font/google";
 import "../globals.css";
 import TranslateButtons from "../ui-components/translateButtons";
@@ -13,13 +14,15 @@ export default function ProjectsLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className + " overflow-hidden"}>
         <div className="absolute w-16 h-12 top-4 right-0">
           <TranslateButtons />
         </div>
         <div className="flex flex-row">
           <Navbar />
-          <div className="basis-4/5">{children}</div>
+          <div className="basis-4/5">
+            <ScrollShadow className="w-full h-screen">{children}</ScrollShadow>
+          </div>
         </div>
       </body>
     </html>
