a2c9ab914f8b038dd60e3aa209910b89b2d7882e
Tanya <roxxxanafray@gmail.com>
2024-07-22 14:28:48 +0300

    made arrows in slider disabled when you can not scroll

--- src/app/projects/ui-components/imagesCarousel.tsx
+++ src/app/projects/ui-components/imagesCarousel.tsx
@@ -1,31 +1,37 @@
 import {Image} from "@nextui-org/image";
 import {Button} from "@nextui-org/react";
-import React from "react";
+import React, {useState} from "react";
 import {ScrollShadow} from "@nextui-org/react";
 import ScreenWidth from "@/app/utils/screenWidth";
 
 export default function ImagesCarousel(props: CarouselProps) {
+    const [sliderScrollPosition, setSliderScrollPosition] = useState(0);
+    const [maxSliderScrollPosition, setMaxSliderScrollPosition] = useState(1);
+    let screenWidth = ScreenWidth();
+
     function scroll(side: string) {
-        let container = document.getElementById("scrollContainer");
-        let leftScrollPosition = container?.scrollLeft || 0;
+        let imagesContainer = document.getElementById("imagesContainer");
+        let scrollContainer = document.getElementById("scrollContainer");
 
-        if (side == "left") {
-            container?.scroll(leftScrollPosition - 600, 0);
-        }
-        if (side == "right") {
-            container?.scroll(leftScrollPosition + 600, 0);
+        if (scrollContainer && imagesContainer) {
+            setSliderScrollPosition(scrollContainer.scrollLeft)
+            setMaxSliderScrollPosition(imagesContainer.offsetWidth - scrollContainer.offsetWidth)
 
-        }
-        if (side == "start") {
-            container?.scroll({top: 0, left: 0, behavior: "instant"});
+            if (side == "left") {
+                scrollContainer.scrollLeft -= 600;
+                setSliderScrollPosition(scrollContainer.scrollLeft - 600)
+            }
+            if (side == "right") {
+                scrollContainer.scrollLeft += 600;
+                setSliderScrollPosition(scrollContainer.scrollLeft + 600)
+            }
         }
     }
-
-    let screenWidth = ScreenWidth();
 
     return (
         <div className="relative flex flex-row justify-start items-center">
             <Button
+                isDisabled={sliderScrollPosition <= 0}
                 variant="light"
                 radius="full"
                 className="hidden xl:flex h-16 text-black/50 absolute xl:-left-[72px]"
@@ -39,6 +45,7 @@ export default function ImagesCarousel(props: CarouselProps) {
                 orientation="horizontal"
                 className="max-w-[550px] mr-4 sm:max-w-[650px] sm:mr-8 md:max-w-[800px] lg:max-w-[1000px] xl:max-w-[600px] 2xl:max-w-[800px] max-h-[300px] scroll-smooth"
                 id="scrollContainer"
+                onScroll={() => scroll("")}
             >
                 <div className="flex flex-row gap-4 w-max" id="imagesContainer">
                     {props.images.map((item, index) => {
@@ -52,22 +59,22 @@ export default function ImagesCarousel(props: CarouselProps) {
                                 className="cursor-pointer object-cover object-center"
                                 onClick={() => {
                                     props.imageClick(index);
-                                    scroll("start")
                                 }}
                             />
                         );
                     })}
                 </div>
             </ScrollShadow>
-            <Button
-                variant="light"
-                radius="full"
-                className="hidden xl:flex h-16 w-16 text-black/50"
-                size="sm"
-                onPress={() => scroll("right")}
-            >
-                <Image src="/images/svg/chevronrightgrey.svg" alt="Close Icon" width={10} height={10}/>
-            </Button>
+                <Button
+                    isDisabled={maxSliderScrollPosition - sliderScrollPosition < 1}
+                    variant="light"
+                    radius="full"
+                    className="hidden xl:flex h-16 w-16 text-black/50"
+                    size="sm"
+                    onPress={() => scroll("right")}
+                >
+                    <Image src="/images/svg/chevronrightgrey.svg" alt="Close Icon" width={10} height={10}/>
+                </Button>
         </div>
     );
 }
