994eb4eea3dc87e4a1d004ac393f551eaee62959
Tanya <roxxxanafray@gmail.com>
2024-07-28 14:07:31 +0300

    changed the title of gamesArray and sliced the description in gameList mobile

--- src/app/utils/getCurrentGameIndex.tsx
+++
@@ -1,6 +0,0 @@
-import projectsList from "../data/gamesArray.json"
-
-export default function GetCurrentGameIndex(gamePathname: string):number {
-    const currentGameIndex = projectsList.findIndex(item => item.link == gamePathname);
-    return currentGameIndex;
-}
